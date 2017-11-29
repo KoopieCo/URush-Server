8320e881e26bc1b721f14e683b26bfbfaf39a3d5
Ryan Nolan <rynolan@uark.edu>
2017-11-29 00:02:11 -0600

    Added ability to store rushee information

---
+++ URushApplication/src/main/java/csce/uark/edu/rynolan/controllers/RegistrationController.java
@@ -0,0 +1,53 @@
+package csce.uark.edu.rynolan.controllers;
+
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import csce.uark.edu.rynolan.configuration.JdbcConfiguration;
+import csce.uark.edu.rynolan.models.Rushee;
+
+@RestController
+public class RegistrationController {
+	
+	@RequestMapping(value = "\registration", method = RequestMethod.POST)
+	public boolean registerRushee(@RequestParam(name = "info", required=true) Rushee rushee) {
+		try {
+			JdbcConfiguration config = new JdbcConfiguration();
+			Connection conn = config.dataSource();
+			
+			String queryString = "INSERT INTO rusheeInfo " + 
+				     "(id, firstName, lastName, email, createdOn)" +
+				     " VALUES " +
+				     "(default, ?, ?, ?, localtimestamp)";
+			
+			PreparedStatement ps = conn.prepareStatement(queryString);
+			if(!rushee.getFirstName().matches("[a-zA-Z]+") || !rushee.getLastName().matches("[a-zA-Z]+")) {
+				System.out.println("Invalid Name");
+				return false;
+			}
+			ps.setString(1, rushee.getFirstName());
+			ps.setString(2, rushee.getLastName());
+			ps.setString(3, rushee.getEmail());
+			
+			ResultSet rs = ps.executeQuery();
+			rs.close();
+			ps.close();
+			config.closeDataSource(conn);
+			return true;
+		}
+		catch(URISyntaxException e) {
+			return false;
+		}
+		catch(SQLException e) {
+			return false;
+		}
+	}
+}
--- URushApplication/src/main/java/csce/uark/edu/rynolan/models/Rushee.java
+++ URushApplication/src/main/java/csce/uark/edu/rynolan/models/Rushee.java
@@ -1,18 +1,22 @@
 package csce.uark.edu.rynolan.models;
 
+import java.sql.Timestamp;
+
 public class Rushee {
 	private int id;
 	private String firstName, lastName;
 	private String universityYear;
 	private String email;
+	private Timestamp timestamp;
 	
-	public Rushee(int id, String firstName, String lastName, String universityYear, String email) {
+	public Rushee(int id, String firstName, String lastName, String universityYear, String email, Timestamp timestamp) {
 		super();
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.universityYear = universityYear;
 		this.email = email;
+		this.setTimestamp(timestamp);
 	}
 	
 	public int getId() {
@@ -44,6 +48,14 @@ public class Rushee {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
 	}
 	
 	

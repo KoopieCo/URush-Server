910b3e453b118922b88dbbaf37e9a61048ae38ca
Ryan Nolan <rynolan@uark.edu>
2017-12-02 13:10:58 -0600

    Fixed TimeStamp format

--- URushApplication/src/main/java/csce/uark/edu/rynolan/controllers/AnnouncementController.java
+++ URushApplication/src/main/java/csce/uark/edu/rynolan/controllers/AnnouncementController.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,12 +42,13 @@ public class AnnouncementController {
 			ResultSet rs = statement.executeQuery(queryString);
 			
 			// Work with Result Set
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 			List<Announcement> announcementList = new ArrayList<>();
 			while(rs.next()) {
 				Announcement temp = new Announcement();
 				temp.setEventTitle(rs.getString(1));
 				temp.setEventLocation(rs.getString(2));
-				temp.setEventTime(rs.getTimestamp(3).toLocalDateTime().toString());
+				temp.setEventTime(rs.getTimestamp(3).toLocalDateTime().format(formatter));
 				
 				announcementList.add(temp);
 			}
